a24e9d342bb1439013b0ed1a76f537423e6a7ddc
Mahi Begoug <gm_begoug@esi.dz>
2020-10-06 15:13:23 +0100

    Test Refactoring

--- tests/AlgerianMobilePhoneNumberCastTest.php
+++ tests/AlgerianMobilePhoneNumberCastTest.php
@@ -9,44 +9,42 @@ use Orchestra\Testbench\TestCase;
 
 class AlgerianMobilePhoneNumberCastTest extends TestCase
 {
+    protected $model;
+    protected $normalized;
+
+    public function setUp() : void
+    {
+        parent::Setup();
+        $this->model = new Customer();
+        $this->model->firstname = 'Cherif';
+        $this->model->lastname = 'Bouchelaghem';
+        $this->model->mobile_phone_number = '00213-6-99-00-00-00';
+        $this->normalized = '00213699000000';
+    }
+
     public function testValueFromValidString()
     {
-        $model = new Customer();
-        $model->firstname = 'Cherif';
-        $model->lastname = 'Bouchelaghem';
-        $model->mobile_phone_number = '00213-6-99-00-00-00';
-        $normalized = '00213699000000';
-        $this->assertInstanceOf(AlgerianMobilePhoneNumber::class,  $model->mobile_phone_number);
-        $this->assertEquals($normalized, (string)$model->mobile_phone_number);
+        $this->assertInstanceOf(AlgerianMobilePhoneNumber::class,  $this->model->mobile_phone_number);
+        $this->assertEquals($this->normalized, (string)$this->model->mobile_phone_number);
     }
 
     public function testValueFromValueObject()
     {
-        $model = new Customer();
-        $model->firstname = 'Cherif';
-        $model->lastname = 'Bouchelaghem';
-        $model->mobile_phone_number = AlgerianMobilePhoneNumber::fromString('00213-6-99-00-00-00');
-        $normalized = '00213699000000';
-        $this->assertInstanceOf(AlgerianMobilePhoneNumber::class,  $model->mobile_phone_number);
-        $this->assertEquals($normalized, (string)$model->mobile_phone_number);
+        $this->model->mobile_phone_number = AlgerianMobilePhoneNumber::fromString('00213-6-99-00-00-00');
+        $this->assertInstanceOf(AlgerianMobilePhoneNumber::class,  $this->model->mobile_phone_number);
+        $this->assertEquals($this->normalized, (string)$this->model->mobile_phone_number);
     }
 
     public function testValueFromNull()
     {
-        $model = new Customer();
-        $model->firstname = 'Cherif';
-        $model->lastname = 'Bouchelaghem';
-        $model->mobile_phone_number = '';
-        $mobileNumber = $model->mobile_phone_number;
+        $this->model->mobile_phone_number = '';
+        $mobileNumber = $this->model->mobile_phone_number;
         $this->assertNull($mobileNumber);
     }
 
     public function testFromInvalidString()
     {
         $this->expectException(InvalidArgumentException::class);
-        $model = new Customer();
-        $model->firstname = 'Cherif';
-        $model->lastname = 'Bouchelaghem';
-        $model->mobile_phone_number = 'foobarbaz';
+        $this->model->mobile_phone_number = 'foobarbaz';
     }
 }
